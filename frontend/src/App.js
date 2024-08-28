e57c1266c1cc710a36fecfd6e8d5105679386499
Yu Sheng <ongusong@gmail.com>
2024-08-28 15:18:46 +0800

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Transcriber from "./Transcriber";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Whisper Transcriber and Translator</h1>
+            <Transcriber />
+        </div>
+    );
+}
+
+export default App;
